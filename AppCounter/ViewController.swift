f9c1887d0cf5e6b2b975c43bf92a218db28b6245
KaznacheevA.S <kaznacheev.a.s@yandex.ru>
2022-09-05 23:23:21 +0300

    Разработка приложение-счётчика, увеличивает значение на 1 при нажатии на кнопку START. Добавил от себя кнопку CLEAR при нажатии на нее счетчик обнуляется

--- AppCounter/ViewController.swift
+++ AppCounter/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var countLabel: UILabel!
+    @IBOutlet private weak var buttomSettings: UIButton!
+    @IBOutlet private weak var clearButton: UIButton!
+    
+    private var isCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countLabel.text = "Значение счётчика: \(isCount)"
+        
+        buttomSettings.layer.cornerRadius = 15
+        buttomSettings.setTitle("START", for: .normal)
+        
+        clearButton.layer.cornerRadius = 15
+        clearButton.tintColor = .white
+        clearButton.setTitle("CLEAR", for: .normal)
     }
 
-
+    // MARK:
+    @IBAction private func Button(_ sender: Any) {
+        isCount += 1
+        
+        countLabel.text = "Значение счётчика: \(isCount)"
+    }
+    
+    @IBAction private func Clear(_ sender: Any) {
+        countLabel.text = "Значение счётчика: 0"
+    }
+    
 }
 
