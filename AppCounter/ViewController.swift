202c97e66a680a5ba3a7106a34ac05a952ed4c02
KaznacheevA.S <kaznacheev.a.s@yandex.ru>
2022-09-10 10:07:37 +0300

    Исправление ошибок по задаче: Убрал не нужный(пустой) //MARK, Поправил имена. Изменил работу кнопки сброса счетчика (если тут я не правильно реализовал прошу подсказать как должно быть)

--- AppCounter/ViewController.swift
+++ AppCounter/ViewController.swift
@@ -10,33 +10,33 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet private weak var countLabel: UILabel!
-    @IBOutlet private weak var buttomSettings: UIButton!
-    @IBOutlet private weak var clearButton: UIButton!
+    @IBOutlet private weak var startCounterButtom: UIButton!
+    @IBOutlet private weak var clearCounterButton: UIButton!
     
-    private var isCount = 0
+    private var counter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        countLabel.text = "Значение счётчика: \(isCount)"
+        countLabel.text = "Значение счётчика: \(counter)"
         
-        buttomSettings.layer.cornerRadius = 15
-        buttomSettings.setTitle("START", for: .normal)
+        startCounterButtom.layer.cornerRadius = 15
+        startCounterButtom.setTitle("START", for: .normal)
         
-        clearButton.layer.cornerRadius = 15
-        clearButton.tintColor = .white
-        clearButton.setTitle("CLEAR", for: .normal)
+        clearCounterButton.layer.cornerRadius = 15
+        clearCounterButton.tintColor = .white
+        clearCounterButton.setTitle("CLEAR", for: .normal)
     }
 
-    // MARK:
-    @IBAction private func Button(_ sender: Any) {
-        isCount += 1
+    @IBAction private func didStartButton(_ sender: Any) {
+        counter += 1
         
-        countLabel.text = "Значение счётчика: \(isCount)"
+        countLabel.text = "Значение счётчика: \(counter)"
     }
     
-    @IBAction private func Clear(_ sender: Any) {
-        countLabel.text = "Значение счётчика: 0"
+    @IBAction private func didClearButton(_ sender: Any) {
+        counter = 0
+        countLabel.text = "Значение счётчика: \(counter)"
     }
     
 }
